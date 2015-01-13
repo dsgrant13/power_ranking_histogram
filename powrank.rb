fda43e6a66e808784a550a7a7b797d76d352bd93
Daniel Grant <daniel.s.grant12@gmail.com>
2015-01-13 15:56:39 -0800

    Prints asterisk histogram with ranking displayed for each week

--- powrank.rb
+++ powrank.rb
@@ -4,6 +4,7 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'chartkick'
 
 
 week = 0
@@ -13,13 +14,18 @@ while week < 19 do
   rankings = Nokogiri::HTML(open("http://espn.go.com/nfl/powerrankings/_/week/#{week}"))
   rankings.css("tr").each do |team|
     if team.css("a").text == "Chargers"
-      puts "Week #{week}"
-      puts team.css("a").text
-      puts team.css(".pr-rank").text
-      chargers[week] = team.css(".pr-rank").text
+      #puts "Week #{week}"
+      #puts team.css("a").text
+      #puts team.css(".pr-rank").text
+      chargers[week] = team.css(".pr-rank").text.to_i
     end
   end
   week += 1
 end
 
-
+week = 0
+puts "Chargers Histogram"
+chargers.each do |rank|
+  puts "Week #{week}:" + "*"*rank + "(#{rank})"
+  week += 1
+end
